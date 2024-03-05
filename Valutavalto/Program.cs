0476d8ec22a69b6a9831a030c861d8a0e2c76040
szabomarcel <szabomarcell34@gmail.com>
2024-03-05 11:57:17 +0100

    Add project files.

---
+++ Valutavalto/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using Valto;
+
+namespace Valutavalto
+{
+    internal class Program
+    {
+        static Valuta valto = null;
+        static async Task Main(string[] args)
+        {
+            List<Valuta> valtos = new List<Valuta>();
+            await valutavaltas();
+            Console.WriteLine("Átváltás: ");
+            Console.ReadLine();
+        }
+
+        private static async Task valutavaltas()
+        {
+            List<Valuta> valto = new List<Valuta>();
+            var client = new HttpClient();            
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://infojegyzet.hu/webszerkesztes/php/valuta/api/v1/arfolyam/");
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
